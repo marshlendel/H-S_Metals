3847bb95eaec63448b53526b67c72f3e3df65fcc
micah-withers <55194246+micah-withers@users.noreply.github.com>
2020-02-19 22:56:36 -0500

    Styles and cleanup. Added licensing.

--- Dashboard/scripts.js
+++ Dashboard/scripts.js
@@ -1,3 +1,13 @@
+// **************************************************************************
+// * Copyright 2020 Marshall Brown, Josiah Schmidt, Micah Withers
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// **************************************************************************
+
 // Get the pop up
 var modal = document.getElementById("addLot");
 
