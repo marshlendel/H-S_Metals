83c951a7fe339d592c05af09329cee5ffedebe75
micah-withers <55194246+micah-withers@users.noreply.github.com>
2020-03-12 10:15:48 -0400

    Update test.php

--- Dashboard/test.php
+++ Dashboard/test.php
@@ -2,9 +2,10 @@
 $servername = "localhost";
 $username = "mwithers";
 $password = "2270410";
+$db = "HandSMetals"
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password, $db);
 
 // Check connection
 if (!$conn) {
@@ -20,20 +21,49 @@ echo "Connected successfully";
 //     echo "Error creating database: " . $conn->error;
 // }
 
-// // sql to create table
-// $sql = "CREATE TABLE MyGuests (
-// id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-// firstname VARCHAR(30) NOT NULL,
-// lastname VARCHAR(30) NOT NULL,
-// email VARCHAR(50),
-// reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-// )";
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+// prepare and bind
+$stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $firstname, $lastname, $email);
+
+// set parameters and execute
+$firstname = "John";
+$lastname = "Doe";
+$email = "john@example.com";
+$stmt->execute();
+
+$firstname = "Mary";
+$lastname = "Moe";
+$email = "mary@example.com";
+$stmt->execute();
+
+echo "New records created successfully";
+
+$stmt->close();
+
+// $sql = "SELECT * FROM table";
 //
-// if ($conn->query($sql) === TRUE) {
-//     echo "Table MyGuests created successfully";
-// } else {
-//     echo "Error creating table: " . $conn->error;
+// function name($sql) {
+//     if ($conn->query($sql) === TRUE) {
+//         echo "Table MyGuests created successfully";
+//     } else {
+//         echo "Error creating table: " . $conn->error;
+//     }
 // }
 
-// $conn->close();
+$conn->close();
 ?>
