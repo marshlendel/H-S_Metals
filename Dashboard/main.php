72bb5e7b2589e45fd507f001c78916eb4241a81d
micah-withers <55194246+micah-withers@users.noreply.github.com>
2020-09-01 18:46:13 -0400

    Format

--- Dashboard/main.php
+++ Dashboard/main.php
@@ -44,58 +44,55 @@
         </style>
         <!-- Trigger/Open The pop up box -->
         <div class="container">
-            <button id="myBtn">Add Lot</button>   
+            <button id="myBtn">Add Lot</button>
             <!-- The pop up -->
             <div id="addLot" class="modal">
 
-              <!-- pop up content -->
-              <div class="modal-content">
-                <span class="close">&times;</span>
-         
-
+            <!-- pop up content -->
+            <div class="modal-content">
+            <span class="close">&times;</span>
                 <!-- form for User Story 3.1 -->
                 <form class="" action="" method="post">
                     <label for="lotnum"><b>Lot No.</b></label>
                     <input type="number" name="lotnum" required>
 					<br></br>
-				 <!-- User story 5.1.1-->
-				<form>
-                    <label for="cust"><b>Customer</b></label>
-                    <select name="cust" id="cust">
-						<option value="Danny">Danny</option>
-						<option value="Dad">Dad</option>
-						<option value="Bobby Darin">Bobby Darin</option>
-					</select>
-				</form>
-				<br></br>
-				<!--User story 5.1.2-->
-				<label><b>Pallet 1</b></label>
-				<br></br>
-                    <label for="amt"><b>Amount</b></label>
-                    <input type="number" name="amt" required>
-				<br></br>
-				<!--User story 5.1.3-->
-				<form>
-                    <label for="type"><b>Type</b></label>
-                    <select name="type" id="type">
-						<option value="Ingot">Ingot</option>
-						<option value="S">S</option>
-						<option value="MS">MS</option>
-					</select>
-				</form>
-				<br></br>
-				<form>
-                    <label for="Status"><b>Status</b></label>
-                    <select name="Status" id="Status">
-						<option value="Dirty">Dirty</option>
-						<option value="Clean">Clean</option>
-						<option value="Finished">Finished</option>
-						<option value="Gone">Gone</option>
-					</select>
-				</form>
+    				 <!-- User story 5.1.1-->
+    				<form>
+                        <label for="cust"><b>Customer</b></label>
+                        <select name="cust" id="cust">
+    						<option value="Danny">Danny</option>
+    						<option value="Dad">Dad</option>
+    						<option value="Bobby Darin">Bobby Darin</option>
+    					</select>
+    				</form>
+    				<br></br>
+    				<!--User story 5.1.2-->
+    				<label><b>Pallet 1</b></label>
+    				<br></br>
+                        <label for="amt"><b>Amount</b></label>
+                        <input type="number" name="amt" required>
+    				<br></br>
+    				<!--User story 5.1.3-->
+    				<form>
+                        <label for="type"><b>Type</b></label>
+                        <select name="type" id="type">
+    						<option value="Ingot">Ingot</option>
+    						<option value="S">S</option>
+    						<option value="MS">MS</option>
+    					</select>
+    				</form>
+    				<br></br>
+    				<form>
+                        <label for="Status"><b>Status</b></label>
+                        <select name="Status" id="Status">
+    						<option value="Dirty">Dirty</option>
+    						<option value="Clean">Clean</option>
+    						<option value="Finished">Finished</option>
+    						<option value="Gone">Gone</option>
+    					</select>
+    				</form>
                     <button type="submit" class="btn">Submit</button>
-					
-                </form>
+				</form>
 
                 <!-- Action for submit of form to add lot (US 3.1)-->
                 <?php if (isset($_POST['lotnum'], $_POST['cust'], ($_POST['amt']))) { ?>
