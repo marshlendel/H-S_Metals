6720c3ec6898ad477cef19108d669ab92c150dbc
HowardPotts98 <60721615+HowardPotts98@users.noreply.github.com>
2020-09-07 14:47:01 -0400

    Update main.php
    trying to add loop for add palet

--- Dashboard/main.php
+++ Dashboard/main.php
@@ -99,10 +99,21 @@
 
 						<!-- script displays pallet 2 for user story -->
 						<script>
-						n = 2
-						function addPallet(n) {
-							document.getElementById("pallets").innerHTML += 
-							"<p><b> Pallet 2 </b></p>	<label for=\"amt\"><b>Amount</b></label><input type=\"number\" name=\"amt\" required><button onclick=\"removePallet()\"> Remove Pallet</button><form><label for=\"type\"><b>Type</b></label><select name=\"type\" id=\"type\"><option value=\"Ingot\">Ingot</option><option value=\"S\">S</option><option value=\"MS\">MS</option></select></form><br></br><form><label for=\"Status\"><b>Status</b></label><select name=\"Status\" id=\"Status\"><option value=\"Dirty\">Dirty</option><option value=\"Clean\">Clean</option><option value=\"Finished\">Finished</option><option value=\"Gone\">Gone</option></select></form>"
+						
+						function addPallet() {
+							 if (typeof(Storage) !== "undefined") {
+								if (localStorage.clickcount) {
+								  localStorage.clickcount = Number(localStorage.clickcount)+1;
+								} else {
+								  localStorage.clickcount = 1;
+								}
+								document.getElementById("result").innerHTML = "Pallet number: ";
+							  } else {
+								document.getElementById("result").innerHTML = "Pallet storage error";
+							  }
+							}
+							// document.getElementById("pallets").innerHTML += 
+							// "<p><b> Pallet 2 </b></p>	<label for=\"amt\"><b>Amount</b></label><input type=\"number\" name=\"amt\" required><button onclick=\"removePallet()\"> Remove Pallet</button><form><label for=\"type\"><b>Type</b></label><select name=\"type\" id=\"type\"><option value=\"Ingot\">Ingot</option><option value=\"S\">S</option><option value=\"MS\">MS</option></select></form><br></br><form><label for=\"Status\"><b>Status</b></label><select name=\"Status\" id=\"Status\"><option value=\"Dirty\">Dirty</option><option value=\"Clean\">Clean</option><option value=\"Finished\">Finished</option><option value=\"Gone\">Gone</option></select></form>"
 						};
 						</script>
 						
