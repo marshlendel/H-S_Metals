9f238c1380e95a02d4210260419e05c095045287
HowardPotts98 <60721615+HowardPotts98@users.noreply.github.com>
2020-09-07 16:40:34 -0400

    Update main.php

--- Dashboard/main.php
+++ Dashboard/main.php
@@ -101,7 +101,7 @@
 						<script>
 						
 						function addPallet() {
-								document.getElementById("pallets").stepUp(1).innerHTML += 
+								document.getElementById("pallets").innerHTML += 
 								"<p><b> Pallet </b></p>	<label for=\"amt\"><b>Amount</b></label><input type=\"number\" name=\"amt\" required><button onclick=\"removePallet()\"> Remove Pallet</button><form><label for=\"type\"><b>Type</b></label><select name=\"type\" id=\"type\"><option value=\"Ingot\">Ingot</option><option value=\"S\">S</option><option value=\"MS\">MS</option></select></form><br></br><form><label for=\"Status\"><b>Status</b></label><select name=\"Status\" id=\"Status\"><option value=\"Dirty\">Dirty</option><option value=\"Clean\">Clean</option><option value=\"Finished\">Finished</option><option value=\"Gone\">Gone</option></select></form>"
 						};
 						</script>
