8f8ad44cc099eda5321bf49e3078344f074a5c99
HowardPotts98 <60721615+HowardPotts98@users.noreply.github.com>
2020-10-27 14:56:37 -0400

    Update main.php
    adding a link for the lot numbers in the table to addLot.php

--- Dashboard/main.php
+++ Dashboard/main.php
@@ -65,6 +65,7 @@ require 'sql.php';
             ?>
             var rows = <?php echo json_encode(get_rows($lots)); ?>;
             var fields = <?php echo json_encode(get_fields($lots))?>;
+			<?php link($lotnum,$lots);?>
             var headers = ["Lot", "Customer", "Gross", "Tare", "Net", "Status"];
             var tableId = "lotTable";
             Script.makeTable(tableId, fields, rows, headers, "lotnum");
